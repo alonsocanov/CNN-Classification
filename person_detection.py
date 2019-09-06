25d96bdcf11b4608f1944c7f33c561bcb47367f9
alonsocanov <alonsocanov@hotmail.com>
2019-09-06 17:35:24 +0200

    Change of var.

--- person_detection.py
+++ person_detection.py
@@ -6,7 +6,6 @@ import variables as var
 import functions as func
 
 
-labels = var.labels
 model = my_model.Net()
 model.eval()
 optimizer = optim.SGD(model.parameters(), lr=var.lr, momentum=var.momentum)
@@ -17,16 +16,15 @@ optimizer.load_state_dict(checkpoint['optimizer'])
 epoch = checkpoint['epoch']
 loss = checkpoint['loss']
 
-transform = var.transform
 
 image_name = ['IMG1.jpg', 'IMG2.jpg', 'IMG3.jpg']
 for img in image_name:
     im = Image.open(img)
     # im.show()
 
-    im_t = transform(im)
+    im_t = var.transform(im)
     func.imshow(im_t, [0.485, 0.456, 0.406], [0.229, 0.224, 0.225], 'Unknown')
 
     outputs = model(torch.unsqueeze(im_t, 0))
     _, predicted = torch.max(outputs, 1)
-    print('Image :', img, ' : ', labels[predicted])
+    print('Image :', img, ' : ', var.labels[predicted])
